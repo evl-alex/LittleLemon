ce2ce6a27c222324240cc3e6991f24f725665f13
Alex Bibik <alexandr.bibik@gmail.com>
2026-03-05 09:49:26 +0200

    mysql setup; booking and menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField(validators=[MaxValueValidator(999999)])
+    booking_date = models.DateTimeField()
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    invertory = models.IntegerField(validators=[MaxValueValidator(99999)])
