4f03ec39605f9cbda8d260aab549b452284de036
Alex Bibik <alexandr.bibik@gmail.com>
2026-03-06 09:40:44 +0200

    added serializers and model views

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Booking, Menu
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ["id", "title", "price", "invertory"]
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
