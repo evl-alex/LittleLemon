4f6ad6752b3703f3436486889e7cdfd286950ff8
Alex Bibik <alexandr.bibik@gmail.com>
2026-03-09 22:21:50 +0200

    Added authorization

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 
@@ -19,5 +20,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
